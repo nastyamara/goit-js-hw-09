4345df91178ed98c474f2d320264aa9520f9e943
nastyamara <110552988+nastyamara@users.noreply.github.com>
2023-03-29 13:39:50 +0000

    Deploying to gh-pages from @ nastyamara/goit-js-hw-09@f893f63b9cd2b83a82f11e0a6afcac1458a01a60 🚀

---
+++ 01-color-switcher.5609ec60.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),a=document.body,d=null;function n(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}e.disabled=!0,t.addEventListener("click",(function(){t.classList.contains("disabled")||(d=setInterval(n,1e3),t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(function(){clearInterval(d),t.classList.remove("disabled"),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.5609ec60.js.map
---
+++ 01-color-switcher.ab3c1f57.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;let a=null;function l(){d.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.disabled=!0,t.addEventListener("click",(()=>{t.classList.contains("disabled")||(a=setInterval(l,1e3),t.disabled=!0,e.disabled=!1)})),e.addEventListener("click",(()=>{clearInterval(a),t.classList.remove("disabled"),t.disabled=!1,e.disabled=!0}));
+//# sourceMappingURL=01-color-switcher.ab3c1f57.js.map
